4f12e654e911b393378af09b589104aa74d55a09
ryan-anderson-ds <52826135+ryan-anderson-ds@users.noreply.github.com>
2024-01-06 16:00:47 -0800

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,8 +1,11 @@
 // Your JavaScript
 
-const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.content');
-document.querySelector('button').onclick = function () {
-  sidebar.classList.toggle('sidebar_small');
-  mainContent.classList.toggle('main-content_expanded')
-}
+window.onload = function(){ 
+  const sidebar = document.querySelector('.sidebar');
+  const mainContent = document.querySelector('.content');
+  document.querySelector('button').onclick = function () {
+    sidebar.classList.toggle('sidebar_small');
+    mainContent.classList.toggle('main-content_expanded')
+  }
+};
+
